a0d12aeed73f74eb5b5ffeef5abd6e1e180dbcf6
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2024-03-18 19:07:52 -0300

    Code improved

--- js/script.js
+++ js/script.js
@@ -8,8 +8,6 @@ const global = {
     totalResults: 0,
   },
   api: {
-    // Register your key at https://www.themoviedb.org/settings/api and enter here
-    // Only use this for development or very small projects. You should store your key and make requests from a server
     apiKey: '34b5e8a3ff03169f1c068a1cbfb64f90',
     apiUrl: 'https://api.themoviedb.org/3/',
   },
