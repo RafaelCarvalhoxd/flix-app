61489d38ba61b29dd4fdacf2ed189268fae6daef
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2024-03-18 15:52:50 -0300

    Add search data

--- js/script.js
+++ js/script.js
@@ -255,14 +255,73 @@ async function search () {
     global.search.term = urlParams.get('search-term');
     
     if(global.search.term !== '' && global.search.type !== null) {
-      const { results } = await searchAPIData ();
-      console.log(results);
+      const { results, total_pages, page } = await searchAPIData ();
+      
+      if (results.length === 0) {
+        showAlert('No results found');
+        return
+      }
+
+      displaySearchResults(results);
+
+      document.querySelector('#search-term').value = '';
+
     } else {
       showAlert('Please enter a search term')
     }
-
-
 };
+
+function displaySearchResults(results) {
+  // Clear previous results
+  document.querySelector('#search-results').innerHTML = '';
+  document.querySelector('#search-results-heading').innerHTML = '';
+  document.querySelector('#pagination').innerHTML = '';
+
+  results.forEach((result) => {
+    const div = document.createElement('div');
+    div.classList.add('card');
+    div.innerHTML = `
+          <a href="${global.search.type}-details.html?id=${result.id}">
+            ${
+              result.poster_path
+                ? `<img
+              src="https://image.tmdb.org/t/p/w500${result.poster_path}"
+              class="card-img-top"
+              alt="${
+                global.search.type === 'movie' ? result.title : result.name
+              }"
+            />`
+                : `<img
+            src="../images/no-image.jpg"
+            class="card-img-top"
+             alt="${
+               global.search.type === 'movie' ? result.title : result.name
+             }"
+          />`
+            }
+          </a>
+          <div class="card-body">
+            <h5 class="card-title">${
+              global.search.type === 'movie' ? result.title : result.name
+            }</h5>
+            <p class="card-text">
+              <small class="text-muted">Release: ${
+                global.search.type === 'movie'
+                  ? result.release_date
+                  : result.first_air_date
+              }</small>
+            </p>
+          </div>
+        `;
+
+    document.querySelector('#search-results-heading').innerHTML = `
+              <h2>${results.length} of ${global.search.totalResults} Results for ${global.search.term}</h2>
+    `;
+
+    document.querySelector('#search-results').appendChild(div);
+  });
+
+}
 
 
 //Display Slider Movies
