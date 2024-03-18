3acef09544200f7369b4ab31fb975be242bbf547
Rafael Carvalho <devrafaelcarvalho14@gmail.com>
2024-03-18 16:37:11 -0300

    Add pagination

--- js/script.js
+++ js/script.js
@@ -255,7 +255,11 @@ async function search () {
     global.search.term = urlParams.get('search-term');
     
     if(global.search.term !== '' && global.search.type !== null) {
-      const { results, total_pages, page } = await searchAPIData ();
+      const { results, total_pages, page, total_results } = await searchAPIData ();
+
+      global.search.totalPages = total_pages;
+      global.search.page = page;
+      global.search.totalResults = total_results;
       
       if (results.length === 0) {
         showAlert('No results found');
@@ -321,8 +325,45 @@ function displaySearchResults(results) {
     document.querySelector('#search-results').appendChild(div);
   });
 
+  displayPagination();
 }
 
+// Create & Display Pagination For Search
+function displayPagination() {
+  const div = document.createElement('div');
+  div.classList.add('pagination');
+  div.innerHTML = `
+  <button class="btn btn-primary" id="prev">Prev</button>
+  <button class="btn btn-primary" id="next">Next</button>
+  <div class="page-counter">Page ${global.search.page} of ${global.search.totalPages}</div>
+  `;
+
+  document.querySelector('#pagination').appendChild(div);
+
+  // Disable prev button if on first page
+  if (global.search.page === 1) {
+    document.querySelector('#prev').disabled = true;
+  }
+
+  // Disable next button if on last page
+  if (global.search.page === global.search.totalPages) {
+    document.querySelector('#next').disabled = true;
+  }
+
+  // Next page
+  document.querySelector('#next').addEventListener('click', async () => {
+    global.search.page++;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+
+  // Prev page
+  document.querySelector('#prev').addEventListener('click', async () => {
+    global.search.page--;
+    const { results, total_pages } = await searchAPIData();
+    displaySearchResults(results);
+  });
+}
 
 //Display Slider Movies
 
@@ -403,7 +444,7 @@ async function searchAPIData (endpoint) {
   showSpinner();
 
   const response = await fetch 
-  (`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}`);
+  (`${API_URL}search/${global.search.type}?api_key=${API_KEY}&language=en-US&query=${global.search.term}&page=${global.search.page}`);
   
   const data = await response.json();
 
